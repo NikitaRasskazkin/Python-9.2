696abf03c56a8979ab54d782911d2c31a036ab95
NikitaRasskazkin <rasskazkin64@gmail.com>
2020-07-29 02:55:09 +0400

    Corrections made

--- maun.py
+++ maun.py
@@ -5,24 +5,54 @@ class YaUploader:
     def __init__(self, file_path: str, token: str):
         self.token = token
         self.file_path = file_path
-        self.file_name = file_path[file_path.rfind('/') + 1:]
-        print(self.file_name)
+        self.path = '/*upload_test/'
 
     def upload(self):
         """Метод загруджает файлы по списку file_list на яндекс диск"""
-        headers = {'Authorization': self.token}
-        params = {'path': f'/{self.file_name}'}
-        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        upload_data = requests.get(url, params=params, headers=headers).json()
-        print(upload_data)
-        upload_url = upload_data['href']
-        with open(self.file_path, 'rb') as f:
-            requests.put(upload_url, f)
-        return 'Файл успешно загружен!'
+
+        def get_upload_data(file_path: str, token: str, path: str):
+            """Функция получает данные запроса, в том числе url на загрузку файла"""
+            file_name = file_path[file_path.rfind('/') + 1:]
+            headers = {'Authorization': token}
+            params = {
+                'path': f'{path}{file_name}',
+                'overwrite': True
+            }
+            url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+            reply = requests.get(url, params=params, headers=headers)
+            return {'reply': reply, 'code': reply.status_code}
+
+        def create_folder(token: str, path: str):
+            """Функция создаёт папку, если она ещё не существует"""
+            headers = {'Authorization': token}
+            params = {'path': path}
+            url = 'https://cloud-api.yandex.net/v1/disk/resources'
+            reply = requests.put(url, params=params, headers=headers)
+            return reply.status_code
+
+        def upload_file(upload_data: dict):
+            """Функция загружает файл на Яндекс Диск"""
+            upload_url = upload_data['href']
+            with open(self.file_path, 'rb') as f:
+                reply = requests.put(upload_url, f)
+                return reply.status_code // 100 == 2
+
+        create_folder(self.token, self.path)
+        reply_data = get_upload_data(self.file_path, self.token, self.path)
+        is_upload = False
+        if reply_data['code'] // 100 == 2:
+            upload_data = reply_data['reply'].json()
+            is_upload = upload_file(upload_data)
+        if is_upload:
+            return 'Файл успешно загружен'
+        else:
+            return 'Произошла ошибка'
 
 
 if __name__ == '__main__':
-    token = 'AgAAAAAT5t7wAADLW0sZ3dQbGEzNrJPmvs0nLWg'
-    file_path = 'files/new_file.txt'
+    # Примеры для ввода: files/new_file.txt | files/Python.webp
+    token = '*******************'
+    file_path = input('Введите путь к файлу: ')
     uploader = YaUploader(file_path, token)
     result = uploader.upload()
+    print(result)
